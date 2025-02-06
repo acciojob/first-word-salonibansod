ea1b610640d977bf74158500e8005a49ef082869
salonibansod <147966433+salonibansod@users.noreply.github.com>
2025-02-06 18:28:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
-  // your code here
+  
+  
+  
+  if (s === "") {
+    return "";
+  }
+
+ 
+  const firstSpaceIndex = s.indexOf(" ");
+
+  
+  return firstSpaceIndex === -1 ? s : s.slice(0, firstSpaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
